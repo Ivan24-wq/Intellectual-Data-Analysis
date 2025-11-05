f58fc6276b8b0ac55b0c400b47d1fe36b93c69ac
Ivan24-wq <iv8747583@gmail.com>
2025-11-05 17:49:56 +0300

    Code

--- Practical work№6/main.py
+++ Practical work№6/main.py
@@ -26,6 +26,12 @@ def train_test(X, y, test_ratio = 0.3, seed = 42):
     train_idx = idx[test_size:]
     return X[train_idx], X[test_idx], y[train_idx], y[test_idx]
 
+
+#Реализация оинейное регресии
+def linear_regresion(X, y):
+    X_b = np.c_[np.ones((X.shape[0], 1)), X]
+    beta = np.linalg.inv(X_b.T @ X_b) @ X_b.T @ y
+
 def predict(X, beta):
     X_b = np.c_[np.ones((X.shape[0], 1)), X]
     return X_b @ beta
@@ -36,4 +42,37 @@ def r2_score(y_true, y_pred):
     return 1 - ss_res / ss_tot
 
 
-#Построение модели
+'''
+Построение модели!
+Экзамены независимы
+'''
+res_1 = {}
+for exam in exam_cols:
+    y = df[exam].values
+    X_train, X_test, y_train, y_test = train_test(X, y)
+    beta = linear_regresion(X_train, y_train)
+    y_pred = predict(X_test, beta)
+    res_1[exam] = r2_score(y_test, y_pred)
+
+#Предположение: экзамены зависят
+res_2 = {}
+y = df['math score'].values
+X_train, X_test, y_train, y_test = train_test(X, y)
+beta_math = linear_regresion(X_test, beta_math)
+res_2['math score'] = r2_score(y_test, y_pred_math)
+
+
+X_read = np.c_[df['math score'].values, X]
+y = df['reading score'].values
+X_train, X_test, y_train, y_test = train_test(X_read, y)
+beta_read = linear_regresion(X_train, y_train)
+y_pred_read = predict(X_test, beta_read)
+res_2['reading score'] = r2_score(y_test, y_pred_read)
+
+
+X_write = np.c_[df[['math score', 'reading score']].values, X]
+y = df['writing score'].values
+X_train, X_test, y_train, y_test = train_test(X_write, y)
+beta_write = linear_regresion(X_train, y_train)
+y_pred_write = predict(X_test, beta_write)
+res_2['writing score'] = r2_score(y_test, y_pred_write)
